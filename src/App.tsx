235a1f4c2b2fcae783314680c8143fe10c455e83
Tim Co <timco92@gmail.com>
2024-10-26 13:38:10 -0700

    sample commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { Suspense } from 'react';
-import { AnimatePresence } from 'framer-motion';
-import { BrowserRouter } from 'react-router-dom';
-import './App.scss';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import Loading from './components/Loading';
-import { Toaster } from 'react-hot-toast';
-import TimRoutes from './features/tim/routes';
-import UserRoutes from './features/user/routes';
-import LoginRoutes from './features/login/routes';
-import { Analytics } from '@vercel/analytics/react';
+import { Suspense } from "react";
+import { AnimatePresence } from "framer-motion";
+import { BrowserRouter } from "react-router-dom";
+import "./App.scss";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Loading from "./components/Loading";
+import { Toaster } from "react-hot-toast";
+import TimRoutes from "./features/tim/routes";
+import UserRoutes from "./features/user/routes";
+import LoginRoutes from "./features/login/routes";
+import { Analytics } from "@vercel/analytics/react";
 
 // import Test from './test';
+// sample commit
 
 const queryClient = new QueryClient();
 
